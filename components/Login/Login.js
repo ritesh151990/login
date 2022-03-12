65b8178ebe201c824327a8ad3271dd2989000f94
ritesh151990 <ritesh151990@gmail.com>
2022-03-12 17:48:54 +0530

    change state

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState,  useReducer } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -36,30 +36,31 @@ const Login = (props) => {
   value :'',
   isValid: null,
  });
-  
-//   useEffect(() => {
-//    const identifier= setTimeout(() => {
- 
-// }, 500);
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } =passwordState;
+  useEffect(() => {
+   const identifier= setTimeout(() => {
+    setFormIsValid(emailIsValid && passwordIsValid );
+}, 500);
 
-// return () =>{
-//   clearTimeout(identifier);
+return () =>{
+  clearTimeout(identifier);
   
-// }; 
-// }, [enteredEmail, enteredPassword]);
+}; 
+}, [emailIsValid , passwordIsValid]);
 
   const emailChangeHandler = (event) => {
   dispatchEmail({type:'USER_INPUT',val: event.target.value});
-    setFormIsValid(
-     event.target.value.includes('@') &&passwordState.isValid
-    );
+    // setFormIsValid(
+    //  event.target.value.includes('@') &&passwordState.isValid
+    // );
 };
 
   const passwordChangeHandler = (event) => {
    dispatchPassword({type:'USER_INPUT',val: event.target.value})
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-    );
+    // setFormIsValid(
+    //   emailState.isValid && event.target.value.trim().length > 6
+    // );
    };
 
   const validateEmailHandler = () => {
